b0367ade0d80d96ed20c8ee4ebacc22539b88491
Ruth Chisom <ndukwe100805043@gmail.com>
2022-10-12 16:53:21 +0100

    bring app config to the top so as to overwrite admin logout

--- educa/settings.py
+++ educa/settings.py
@@ -31,14 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'courses.apps.CoursesConfig',
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'courses.apps.CoursesConfig',
-    'accounts.apps.AccountsConfig'
 ]
 
 MIDDLEWARE = [
